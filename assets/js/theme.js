79d22bdca612be4adc05a794df7363b1279bcf14
Abubakar Yagoub <Abubakaryagob@gmail.com>
2018-10-28 14:21:23 +0800

    header: animate drop-down menu's
    Signed-off-by: Abubakar Yagoub <Abubakaryagob@gmail.com>

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -151,3 +151,10 @@ jQuery(document).ready(function($){
 
 
 });
+$('.dropdown').on('show.bs.dropdown', function(e){
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+});
+
+$('.dropdown').on('hide.bs.dropdown', function(e){
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
+});
